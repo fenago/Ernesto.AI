bfd496accf0150ac2c5829d07368c21d3336afa4
OmParkash <omparkash.bhimani@lovefordata.com>
2022-01-24 14:27:33 +0500

    Updated static folder path

--- backend/scrappers_Api/job_scheduler_api/job_scheduler_api/urls.py
+++ backend/scrappers_Api/job_scheduler_api/job_scheduler_api/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', obtain_jwt_token, name='obtain_jwt_token'),
     path('api/', include('api.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
